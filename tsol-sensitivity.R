d458d8beee37a8fd96881e816301d0f854b891ba
Brecht Devleesschauwer <brechtdv@gmail.com>
2015-11-20 11:53:52 -0500

    Add tornado graph

--- tsol-sensitivity.R
+++ tsol-sensitivity.R
@@ -1,5 +1,5 @@
 ### TAENIA SOLIUM IN TANZANIA - SENSITIVITY ANALYSES
-### last update: 14/11/2015
+### last update: 20/11/2015
 
 ###
 ### SENSITIVITY ANALYSIS FUNCTIONS
@@ -27,7 +27,46 @@ function(y, x) {
              use.names = FALSE)
   }
 
-  return(out[order(out[, "rho"], decreasing = TRUE), ])
+  return(out[order(abs(out[, "rho"]), decreasing = TRUE), ])
+}
+
+
+###
+### TORNADO GRAPH
+###
+
+## requires library 'ggplot2'
+library(ggplot2)
+
+## ggplot2
+tornado <-
+function(coef, names = NULL) {
+  ## copy names if needed
+  if (is.null(names)) names <- rownames(coef)
+
+  ## create data frame
+  df <- data.frame(est = coef[, "rho"],
+                   order = order(abs(coef[, "rho"])),
+                   name = names)
+
+  ## sort data frame
+  df <- df[df$order, ]
+
+  ## create ggplot
+  ggplot(df, aes(x = order, y = est)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    scale_x_continuous(element_blank(),
+                       breaks = seq(nrow(df)),
+                       labels = df$name) +
+    scale_y_continuous("partial correlation coefficient",
+                       limits = c(min(0, min(df$est) - 0.1),
+                                  max(0, max(df$est) + 0.1))) +
+    geom_text(aes(x = order, y = est, label = formatC(est, 3, form = "f")),
+              size = 3,
+              hjust = ifelse(df$est > 0, -0.1, 1.1),
+              vjust = 0.4) +
+    theme_bw()
 }
 
 
@@ -56,11 +95,24 @@ sa_pcc(cost_ncc,
              working_days, loss_workingtime, monthly_salary))
 
 ## total
+sa_cost_pcc <-
 sa_pcc(cost_total,
        cbind(prev_pigs, price_pigs,
              e_prev, prop_ncc,
              p_hosp, n_visit_med, price_heal, p_carba,
              working_days, loss_workingtime, monthly_salary))
+sa_cost_pcc
+
+## tornado graph
+cost_items <-
+c("Monthly salary", "Prevalence of E", "Proportion of NCC-associated E", 
+  "Cost of a pig", "Prevalence of PC", "Working days", "Woking time lost", 
+  "Proportion of hospitalizations", "Cost for a traditional healer", 
+  "No of visits to a medical doctor", "Proportion receiving medication")
+
+png("sa_cost_pcc.png", 7, 3.5, units = "in", res = 400)
+tornado(sa_cost_pcc, cost_items)
+graphics.off()
 
 
 ###
@@ -73,5 +125,18 @@ source("tsol-daly.R")
 ## total
 sa_src(daly_all,
        cbind(e_prev, prop_ncc, ep_trt, ep_dsw_tr, ep_dsw_nt, ep_cfr))
+
+sa_daly_pcc <-
 sa_pcc(daly_all,
        cbind(e_prev, prop_ncc, ep_trt, ep_dsw_tr, ep_dsw_nt, ep_cfr))
+sa_daly_pcc
+
+## tornado graph
+daly_items <-
+c("E prevalence", "Proportion of NCC-associated E",
+  "E case-fatality ratio", "E disability weight (untreated)",
+  "E disability weight (treated)", "Treatment proportion")
+
+png("sa_daly_pcc.png", 7, 2.5, units = "in", res = 400)
+tornado(sa_daly_pcc, daly_items)
+graphics.off()
